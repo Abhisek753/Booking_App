94df0b9c76a398cda2fb22684d7b1023800ee65a
Abhisek753 <properachievement@gmail.com>
2023-12-31 12:23:58 +0530

    boolking

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -7,6 +7,16 @@ const List = () => {
     <div>
       <Navbar/>
       <Header type="list"/>
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="lsTitle">Search</h1>
+            <div className="listResult">
+              
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
